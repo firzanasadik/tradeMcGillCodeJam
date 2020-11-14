abb6c7cc8ab849350ff91be8933899f20aa629dc
Calla Lu <46143943+clu2001@users.noreply.github.com>
2020-11-14 17:57:37 -0500

    set up NotFound page handling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,27 @@ import Navbar from "react-bootstrap/Navbar";
 import "./App.css";
 import Routes from "./Routes";
 import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+        {/*links back to home page */}
         <Navbar.Brand href="/" className="font-weight-bold text-muted">
           McGill Trading
         </Navbar.Brand>
         <Navbar.Toggle />
+        {/* navbar.collapse ensures that on mobile devices the two links will be collapse */}
         <Navbar.Collapse className="justify-content-end">
-          <Nav>
-            <Nav.Link href="/signup">Signup</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
+          {/*Routes it to the new link without refreshing the page */}
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link href="/signup">Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link href="/login">Login</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
