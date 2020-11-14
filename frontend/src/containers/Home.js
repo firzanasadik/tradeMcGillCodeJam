9d1255983bb2cd829bef95e08fc402d476769a8f
Calla Lu <46143943+clu2001@users.noreply.github.com>
2020-11-14 17:32:04 -0500

    home page + bootstrap set up

---
+++ frontend/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>McGill Trading</h1>
+        <p className="text-muted">A place where McGill students can trade and barter</p>
+      </div>
+    </div>
+  );
+}
