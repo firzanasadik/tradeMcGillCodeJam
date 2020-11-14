987861036743c2fbd695dd8eea92027a8fc073ec
sophearah <sophearah.suy-puth@protastudios.com>
2020-11-14 14:30:59 -0800

    basic item model and api endpoints

--- backend/trademcgill/admin.py
+++ backend/trademcgill/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Item
+
+class ItemAdmin(admin.ModelAdmin):
+  list_display = ('title', 'description', 'category')
 
 # Register your models here.
+admin.site.register(Item, ItemAdmin) # add this
